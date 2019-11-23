aae7c39623c5bfc2f41099f27aae65b684bf73c0
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2019-11-23 20:30:09 +0100

    [tests] Add more port declaration tests

--- tests/parser/tportdeclarations.nim
+++ tests/parser/tportdeclarations.nim
@@ -1,9 +1,11 @@
 import terminal
 import strformat
+import strutils
 
 import ../../src/parser/parser
 import ../../src/parser/ast
 import ../../src/lexer/identifier
+import ../../src/lexer/lexer
 
 var
    nof_passed = 0
@@ -45,6 +47,7 @@ run_test("Single port (input)", """(
       ])
    ])
 
+
 run_test("Single port (inout)", """(
    inout data_io
 )"""):
@@ -54,6 +57,7 @@ run_test("Single port (inout)", """(
          new_identifier_node(NtPortIdentifier, li(2, 10), "data_io"),
       ])
    ])
+
 
 run_test("Single port (output)", """(
    output data_o
@@ -65,8 +69,105 @@ run_test("Single port (output)", """(
       ])
    ])
 
-run_test("Single port (empty)", "()"):
+
+run_test("Empty list", "()"):
    new_node(NtListOfPortDeclarations, li(1, 2))
+
+
+run_test("Multilple ports, mixed direction", """(
+   input clk_i,
+   inout data_io,
+   output data_o
+)"""):
+   new_node(NtListOfPortDeclarations, li(2, 4), @[
+      new_node(NtPortDecl, li(2, 4), @[
+         new_identifier_node(NtDirection, li(2, 4), "input"),
+         new_identifier_node(NtPortIdentifier, li(2, 10), "clk_i"),
+      ]),
+      new_node(NtPortDecl, li(3, 4), @[
+         new_identifier_node(NtDirection, li(3, 4), "inout"),
+         new_identifier_node(NtPortIdentifier, li(3, 10), "data_io"),
+      ]),
+      new_node(NtPortDecl, li(4, 4), @[
+         new_identifier_node(NtDirection, li(4, 4), "output"),
+         new_identifier_node(NtPortIdentifier, li(4, 11), "data_o"),
+      ])
+   ])
+
+
+run_test("Multilple ports, missing comma", """(
+   input clk_i
+   inout data_io
+)"""):
+   new_node(NtListOfPortDeclarations, li(2, 4), @[
+      new_node(NtPortDecl, li(2, 4), @[
+         new_identifier_node(NtDirection, li(2, 4), "input"),
+         new_identifier_node(NtPortIdentifier, li(2, 10), "clk_i"),
+      ]),
+      new_error_node(li(3, 4), "")
+   ])
+
+# FIXME: Tests w/ attribute instances when that's implemented.
+
+for direction in [TkInput, TkInout, TkOutput]:
+   for net_type in NetTypeTokens:
+      run_test(format("$1 port, net type $2", direction, net_type), format("""(
+   $1
+   $2
+   my_port
+)""", TokenTypeToStr[direction], TokenTypeToStr[net_type])):
+         new_node(NtListOfPortDeclarations, li(2, 4), @[
+            new_node(NtPortDecl, li(2, 4), @[
+               new_identifier_node(NtDirection, li(2, 4), TokenTypeToStr[direction]),
+               new_identifier_node(NtNetType, li(3, 4), TokenTypeToStr[net_type]),
+               new_identifier_node(NtPortIdentifier, li(4, 4), "my_port"),
+            ])
+         ])
+
+
+run_test("Signed ports", """(
+   input signed signed_input,
+   inout signed signed_inout,
+   output signed signed_output,
+   input wire signed signed_input_wire,
+   inout wire signed signed_inout_wire,
+   output wire signed signed_output_wire
+)"""):
+   new_node(NtListOfPortDeclarations, li(2, 4), @[
+      new_node(NtPortDecl, li(2, 4), @[
+         new_identifier_node(NtDirection, li(2, 4), "input"),
+         new_identifier_node(NtType, li(2, 10), "signed"),
+         new_identifier_node(NtPortIdentifier, li(2, 17), "signed_input"),
+      ]),
+      new_node(NtPortDecl, li(3, 4), @[
+         new_identifier_node(NtDirection, li(3, 4), "inout"),
+         new_identifier_node(NtType, li(3, 10), "signed"),
+         new_identifier_node(NtPortIdentifier, li(3, 17), "signed_inout"),
+      ]),
+      new_node(NtPortDecl, li(4, 4), @[
+         new_identifier_node(NtDirection, li(4, 4), "output"),
+         new_identifier_node(NtType, li(4, 11), "signed"),
+         new_identifier_node(NtPortIdentifier, li(4, 18), "signed_output"),
+      ]),
+      new_node(NtPortDecl, li(5, 4), @[
+         new_identifier_node(NtDirection, li(5, 4), "input"),
+         new_identifier_node(NtNetType, li(5, 10), "wire"),
+         new_identifier_node(NtType, li(5, 15), "signed"),
+         new_identifier_node(NtPortIdentifier, li(5, 22), "signed_input_wire"),
+      ]),
+      new_node(NtPortDecl, li(6, 4), @[
+         new_identifier_node(NtDirection, li(6, 4), "inout"),
+         new_identifier_node(NtNetType, li(6, 10), "wire"),
+         new_identifier_node(NtType, li(6, 15), "signed"),
+         new_identifier_node(NtPortIdentifier, li(6, 22), "signed_inout_wire"),
+      ]),
+      new_node(NtPortDecl, li(7, 4), @[
+         new_identifier_node(NtDirection, li(7, 4), "output"),
+         new_identifier_node(NtNetType, li(7, 11), "wire"),
+         new_identifier_node(NtType, li(7, 16), "signed"),
+         new_identifier_node(NtPortIdentifier, li(7, 23), "signed_output_wire"),
+      ])
+   ])
 
 
 # Print summary
