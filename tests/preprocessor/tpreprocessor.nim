437924e3b945864f0fe482b1c2804499e1c3dbb7
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-04-08 22:57:39 +0200

    [tests] Add preprocessor test cases

--- tests/preprocessor/tpreprocessor.nim
+++ tests/preprocessor/tpreprocessor.nim
@@ -41,60 +41,164 @@ proc new_identifier(kind: TokenKind, line, col: int, identifier: string): Token 
    new_identifier(kind, line, col, identifier, cache)
 
 
-# run_test("Default", """
-# HELLO
-# `define aMAZing FOO
-# `aMAZing
-# `define MYMACRO ahs delightful `aMAZing \
-#    `aMAZing
-# this is `MYMACRO
-# `define aMAZing bar
-# testing `MYMACRO
-# """): [
-#    new_identifier(TkSymbol, 1, 0, "HELLO"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-#    new_identifier(TkModule, 4, 0, "module"),
-# ]
+run_test("Object-like macro", """
+`define WIRE wire [7:0]
+`WIRE my_wire;
+"""): [
+   new_identifier(TkWire, 1, 13, "wire"),
+   new_token(TkLbracket, 1, 18),
+   new_inumber(TkIntLit, 1, 19, 7, Base10, -1, "7"),
+   new_token(TkColon, 1, 20),
+   new_inumber(TkIntLit, 1, 21, 0, Base10, -1, "0"),
+   new_token(TkRbracket, 1, 22),
+   new_identifier(TkSymbol, 2, 6, "my_wire"),
+   new_token(TkSemicolon, 2, 13),
+]
 
 
-run_test("Nested arguments", """
+run_test("Nested object-like macro", """
+`define HIGH 7
+`define WIRE wire [`HIGH:0]
+`WIRE my_wire;
+"""): [
+   new_identifier(TkWire, 2,  13, "wire"),
+   new_token(TkLbracket, 2,  18),
+   new_inumber(TkIntLit, 1,  13, 7, Base10, -1, "7"),
+   new_token(TkColon, 2,  24),
+   new_inumber(TkIntLit, 2,  25, 0, Base10, -1,  "0"),
+   new_token(TkRbracket, 2,  26),
+   new_identifier(TkSymbol, 3, 6, "my_wire"),
+   new_token(TkSemicolon, 3, 13),
+]
+
+
+run_test("Nested object-like macros, immediate expansion", """
+`define FOO foo
+`define BAR `FOO
+`BAR
+"""): [
+   new_identifier(TkSymbol, 1, 12, "foo"),
+]
+
+
+run_test("Nested object-like macros, reverse order of definition", """
+`define BAR `FOO
+`define FOO foo
+`BAR
+"""): [
+   new_identifier(TkSymbol, 2, 12, "foo"),
+]
+
+
+run_test("Propagate unknown directive tokens", """
+`FOO
+"""): [
+   new_identifier(TkDirective, 1, 0, "FOO"),
+]
+
+
+run_test("Object-like macro, redefinition", """
+`define WIRE wire [7:0]
+`WIRE my_wire;
+`define WIRE wire [1:0]
+`WIRE smaller_wire;
+"""): [
+   new_identifier(TkWire, 1, 13, "wire"),
+   new_token(TkLbracket, 1, 18),
+   new_inumber(TkIntLit, 1, 19, 7, Base10, -1, "7"),
+   new_token(TkColon, 1, 20),
+   new_inumber(TkIntLit, 1, 21, 0, Base10, -1, "0"),
+   new_token(TkRbracket, 1, 22),
+   new_identifier(TkSymbol, 2, 6, "my_wire"),
+   new_token(TkSemicolon, 2, 13),
+   new_identifier(TkWire, 3, 13, "wire"),
+   new_token(TkLbracket, 3, 18),
+   new_inumber(TkIntLit, 3, 19, 1, Base10, -1, "1"),
+   new_token(TkColon, 3, 20),
+   new_inumber(TkIntLit, 3, 21, 0, Base10, -1, "0"),
+   new_token(TkRbracket, 3, 22),
+   new_identifier(TkSymbol, 4, 6, "smaller_wire"),
+   new_token(TkSemicolon, 4, 18),
+]
+
+run_test("`undef macro before usage", """
+`define WIRE wire [7:0]
+`WIRE my_wire;
+`undef WIRE
+`WIRE smaller_wire;
+"""): [
+   new_identifier(TkWire, 1, 13, "wire"),
+   new_token(TkLbracket, 1, 18),
+   new_inumber(TkIntLit, 1, 19, 7, Base10, -1, "7"),
+   new_token(TkColon, 1, 20),
+   new_inumber(TkIntLit, 1, 21, 0, Base10, -1, "0"),
+   new_token(TkRbracket, 1, 22),
+   new_identifier(TkSymbol, 2, 6, "my_wire"),
+   new_token(TkSemicolon, 2, 13),
+   new_identifier(TkDirective, 4, 0, "WIRE"),
+   new_identifier(TkSymbol, 4, 6, "smaller_wire"),
+   new_token(TkSemicolon, 4, 18),
+]
+
+
+run_test("Function-like macro", """
+`define REG(width) reg [width:0]
+`REG(7) a_reg;
+"""): [
+   new_identifier(TkReg, 1, 19, "reg"),
+   new_token(TkLbracket, 1, 23),
+   new_inumber(TkIntLit, 2, 5, 7, Base10, -1, "7"),
+   new_token(TkColon, 1, 29),
+   new_inumber(TkIntLit, 1, 30, 0, Base10, -1, "0"),
+   new_token(TkRbracket, 1, 31),
+   new_identifier(TkSymbol, 2, 8, "a_reg"),
+   new_token(TkSemicolon, 2, 13),
+]
+
+
+run_test("Incorrect spacing in function-like macro -> object-like", """
+`define NOT_A_FUNCTION_MACRO (value) {(value){1'b0}}
+`NOT_A_FUNCTION_MACRO(8);
+"""): [
+   new_token(TkLparen, 1, 29),
+   new_identifier(TkSymbol, 1, 30, "value"),
+   new_token(TkRparen, 1, 35),
+   new_token(TkLbrace, 1, 37),
+   new_token(TkLparen, 1, 38),
+   new_identifier(TkSymbol, 1, 39, "value"),
+   new_token(TkRparen, 1, 44),
+   new_token(TkLbrace, 1, 45),
+   new_inumber(TkUIntLit, 1, 46, 0, Base2, 1, "0"),
+   new_token(TkRbrace, 1, 50),
+   new_token(TkRbrace, 1, 51),
+   new_token(TkLparen, 2, 21),
+   new_inumber(TkIntLit, 2, 22, 8, Base10, -1, "8"),
+   new_token(TkRparen, 2, 23),
+   new_token(TkSemicolon, 2, 24),
+]
+
+
+run_test("Nested function- & object-like macros", """
 `define CONSTANT ABC
 `define bar(x, y) foo `CONSTANT x y
 `bar(`bar(3, `CONSTANT), bAz) (2)
 """): [
-   new_identifier(TkSymbol, 3, 0, "foo"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
-   new_identifier(TkModule, 4, 0, "module"),
+   new_identifier(TkSymbol, 2, 18, "foo"),
+   new_identifier(TkSymbol, 1, 17, "ABC"),
+   new_identifier(TkSymbol, 2, 18, "foo"),
+   new_identifier(TkSymbol, 1, 17, "ABC"),
+   new_inumber(TkIntLit, 3, 10, 3, Base10, -1, "3"),
+   new_identifier(TkSymbol, 1, 17, "ABC"),
+   new_identifier(TkSymbol, 3, 25, "bAz"),
+   new_token(TkLparen, 3, 30),
+   new_inumber(TkIntLit, 3, 31, 2, Base10, -1, "2"),
+   new_token(TkRparen, 3, 32),
 ]
 
+
+# TODO: Test `undef, expect error.
+# TODO: Test number of arguments mismatch: fewer, more.
+# TODO: Test directive pass-through to parser.
 
 # Print summary
 styledWriteLine(stdout, styleBright, "\n----- SUMMARY -----")
--- vparse.nimble
+++ vparse.nimble
@@ -12,15 +12,20 @@ requires "nim >= 1.2.0"
 
 task test, "Run the test suite":
    exec("nimble lexertests")
+   exec("nimble preprocessortests")
    exec("nimble parsertests")
 
 task lexertests, "Run the lexer test suite":
-   withDir("tests/lexer"):
+   with_dir("tests/lexer"):
       exec("nim c --hints:off -r tidentifier")
       exec("nim c --hints:off -r tlexer")
 
+task preprocessortests, "Run the preprocessor test suite":
+   with_dir("tests/preprocessor"):
+      exec("nim c --hints:off -r tpreprocessor")
+
 task parsertests, "Run the parser test suite":
-   withDir("tests/parser"):
+   with_dir("tests/parser"):
       exec("nim c --hints:off -r tconstantexpression")
       exec("nim c --hints:off -r tportlist")
       exec("nim c --hints:off -r tportdeclarations")
