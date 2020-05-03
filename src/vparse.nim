4e920f51cc72f0ff772b7e045cf3edc23cff0cef
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-05-03 17:56:38 +0200

    [prog] Don't include main program code when included as a library

--- src/vparse.nim
+++ src/vparse.nim
@@ -7,99 +7,102 @@ import times
 import terminal
 import json
 
-const
-   # Version information
-   VERSION_STR = static_read("./vparsepkg/private/VERSION").strip()
-   # Exit codes: negative values are errors.
-   ESUCCESS = 0
-   EINVAL = 1
-   EFILE = 2
-   EPARSE = 3
+export graph
 
-   STATIC_HELP_TEXT = static_read("./vparsepkg/private/CLI_HELP")
+when is_main_module:
+   const
+      # Version information
+      VERSION_STR = static_read("./vparsepkg/private/VERSION").strip()
+      # Exit codes: negative values are errors.
+      ESUCCESS = 0
+      EINVAL = 1
+      EFILE = 2
+      EPARSE = 3
 
-let HELP_TEXT = "vparse v" & VERSION_STR & "\n\n" & STATIC_HELP_TEXT
+      STATIC_HELP_TEXT = static_read("./vparsepkg/private/CLI_HELP")
 
-# Parse the arguments and options and return a CLI state object.
-var cli_state: CLIState
-try:
-   cli_state = parse_cli()
-except CLIValueError:
-   quit(-EINVAL)
+   let HELP_TEXT = "vparse v" & VERSION_STR & "\n\n" & STATIC_HELP_TEXT
 
-# Parse CLI object state.
-if not cli_state.is_ok:
-   # Invalid input combination (but otherwise correctly formatted arguments
-   # and options).
-   echo HELP_TEXT
-   quit(-EINVAL)
-elif cli_state.print_help:
-   # Show help text and exit.
-   echo HELP_TEXT
-   quit(ESUCCESS)
-elif cli_state.print_version:
-   # Show version information and exit.
-   echo VERSION_STR
-   quit(ESUCCESS)
+   # Parse the arguments and options and return a CLI state object.
+   var cli_state: CLIState
+   try:
+      cli_state = parse_cli()
+   except CLIValueError:
+      quit(-EINVAL)
 
-log.info("vparse v" & VERSION_STR)
+   # Parse CLI object state.
+   if not cli_state.is_ok:
+      # Invalid input combination (but otherwise correctly formatted arguments
+      # and options).
+      echo HELP_TEXT
+      quit(-EINVAL)
+   elif cli_state.print_help:
+      # Show help text and exit.
+      echo HELP_TEXT
+      quit(ESUCCESS)
+   elif cli_state.print_version:
+      # Show version information and exit.
+      echo VERSION_STR
+      quit(ESUCCESS)
 
-if len(cli_state.input_files) == 0:
-   log.error("No input files, aborting.")
-   quit(-EINVAL)
+   log.info("vparse v" & VERSION_STR)
 
-var ofs: FileStream = nil
-if len(cli_state.output_file) != 0:
-   ofs = new_file_stream(cli_state.output_file, fmWrite)
-   if ofs == nil:
-      log.warning("Failed to open output file '$1' for writing.",
-                  cli_state.output_file)
-   else:
-      log.info("Opened output file '$1'.", cli_state.output_file)
+   if len(cli_state.input_files) == 0:
+      log.error("No input files, aborting.")
+      quit(-EINVAL)
 
-var g: Graph
-var exit_val = ESUCCESS
-for filename in cli_state.input_files:
-   let fs = new_file_stream(filename)
-   if fs == nil:
-      log.error("Failed to open '$1' for reading, skipping.", filename)
-      continue
-
-   let cache = new_ident_cache()
-   open_graph(g, cache, fs, filename, cli_state.include_paths,
-              cli_state.external_defines)
-   log.info("Parsing source file '$1'", filename)
-
-   let t_start = cpu_time()
-   let root_node = parse_all(g)
-   let t_diff_ms = (cpu_time() - t_start) * 1000
-   close_graph(g)
-   close(fs)
-
-   if ofs != nil:
-      if cli_state.json:
-         if cli_state.pretty:
-            ofs.write(pretty(%root_node))
-         else:
-            ofs.write($(%root_node))
+   var ofs: FileStream = nil
+   if len(cli_state.output_file) != 0:
+      ofs = new_file_stream(cli_state.output_file, fmWrite)
+      if ofs == nil:
+         log.warning("Failed to open output file '$1' for writing.",
+                     cli_state.output_file)
       else:
-         ofs.write(pretty(root_node))
+         log.info("Opened output file '$1'.", cli_state.output_file)
 
-   if cli_state.stdout:
-      if cli_state.json:
-         if cli_state.pretty:
-            echo pretty(%root_node)
+   var g: Graph
+   var exit_val = ESUCCESS
+   for filename in cli_state.input_files:
+      let fs = new_file_stream(filename)
+      if fs == nil:
+         log.error("Failed to open '$1' for reading, skipping.", filename)
+         continue
+
+      let cache = new_ident_cache()
+      open_graph(g, cache, fs, filename, cli_state.include_paths,
+                 cli_state.external_defines)
+      log.info("Parsing source file '$1'", filename)
+
+      let t_start = cpu_time()
+      let root_node = parse_all(g)
+      let t_diff_ms = (cpu_time() - t_start) * 1000
+      close_graph(g)
+      close(fs)
+
+      if ofs != nil:
+         if cli_state.json:
+            if cli_state.pretty:
+               ofs.write(pretty(%root_node))
+            else:
+               ofs.write($(%root_node))
          else:
-            echo $(%root_node)
-      else:
-         echo pretty(root_node)
+            ofs.write(pretty(root_node))
 
-   log.info("Parse completed in ", fgGreen, styleBright,
-            format_float(t_diff_ms, ffDecimal, 1), " ms", resetStyle, ".")
+      if cli_state.stdout:
+         if cli_state.json:
+            if cli_state.pretty:
+               echo pretty(%root_node)
+            else:
+               echo $(%root_node)
+         else:
+            echo pretty(root_node)
 
-   # TODO: Analyze errors and present a summary.
-   if has_errors(root_node):
-      log.error("The AST contains errors.")
-      exit_val = -EPARSE
+      log.info("Parse completed in ", fgGreen, styleBright,
+               format_float(t_diff_ms, ffDecimal, 1), " ms", resetStyle, ".")
 
-quit(exit_val)
+      # TODO: Analyze errors and present a summary.
+      if has_errors(root_node):
+         log.error("The AST contains errors.")
+         exit_val = -EPARSE
+
+   quit(exit_val)
