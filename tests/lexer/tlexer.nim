613af3864def426d4b3ca4c0cff485e0407bbe7c
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-06-30 19:10:34 +0200

    [tests] Remove debug print

--- tests/lexer/tlexer.nim
+++ tests/lexer/tlexer.nim
@@ -62,7 +62,7 @@ run_test("One line comment", """
 // ** This is a one line comment **
 """, @[
    new_comment(TkComment, loc(1, 1, 0), " ** This is a one line comment **")
-], true)
+])
 
 
 run_test("Multi line comment", """
